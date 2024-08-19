75022bc12ca5b82cf8d8bd17a8b7ddd80c779e8a
adg1126 <gutierrez.almiradyan@gmail.com>
2024-08-19 13:50:44 +0800

    Added assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,16 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { ThemedView } from '@/components/ThemedView';
-import { ThemedText } from '@/components/ThemedText';
-import { StyleSheet } from 'react-native';
+
 import { Slot } from 'expo-router';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 
 export default function index() {
   return (
     <View className='flex-1 items-center justify-center'>
-      <Text>Vuro</Text>
+      <Text className='text-3xl'>Vuro</Text>
     </View>
   );
 }

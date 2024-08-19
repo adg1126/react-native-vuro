9a964852d801ce694ae747d386f2269002da170d
adg1126 <gutierrez.almiradyan@gmail.com>
2024-08-19 13:30:08 +0800

    Integrated NativeWind and Tailwind CSS

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function index() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text>Vuro</Text>
+    </View>
+  );
+}

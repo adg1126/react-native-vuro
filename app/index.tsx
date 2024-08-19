e5043cff0c8eef51440c55f50cea5fa8386a9c98
adg1126 <gutierrez.almiradyan@gmail.com>
2024-08-19 14:51:51 +0800

    Setup folder structure and setup bottom tab bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function index() {
   return (
     <View className='flex-1 items-center justify-center'>
-      <Text className='text-3xl'>Vuro</Text>
+      <Text className='text-3xl font-pblack'>Vuro</Text>
+      <StatusBar />
+      <Link href='/(tabs)/home'>Home</Link>
     </View>
   );
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,9 @@
 declare module '*.jpg';
 declare module '*.jpeg';
+
+declare interface TabIconProps {
+  icon: number;
+  color: string;
+  name: string;
+  focused: boolean;
+}

b397ce52f55b9b15df7dae0cd015f1b36e59c490
adg1126 <gutierrez.almiradyan@gmail.com>
2024-08-24 09:26:00 +0800

    Wired up fetchData hook and styled the component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,81 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
+import SearchInput from '@/components/SearchInput';
+import Trending from '@/components/Trending';
+import EmptyState from '@/components/EmptyState';
+import { getAllVideos } from '@/lib/appwrite';
+import { useAppwrite } from '@/hooks/useAppwrite';
+import VideoCard from '@/components/VideoCard';
 
-export default function Page() {
+export default function Home() {
+  const [searchTerm, setSearchTerm] = useState<string>(''),
+    [refresing, setRefresing] = useState<boolean>(false);
+
+  const { data: videos, refetch } = useAppwrite(getAllVideos);
+
+  const handleChangeText = (e: string) => {};
+
+  const onRefresh = async () => {
+    setRefresing(true);
+    await refetch();
+    setRefresing(false);
+  };
+
   return (
-    <View className='flex flex-1 items-center justify-center'>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className='bg-primary'>
+      <FlatList
+        data={videos}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={{ ...item }} />}
+        ListHeaderComponent={() => (
+          <View className='flex my-6 px-4 space-y-6'>
+            <View className='flex justify-between items-start flex-row mb-6'>
+              <View>
+                <Text className='font-pmedium text-sm text-gray-100'>
+                  Welcome back
+                </Text>
+                <Text className='font-psemibold text-2xl text-white'>User</Text>
+              </View>
+
+              <View className='mt-1.5'>
+                <Image
+                  source={images.logo}
+                  className='w-9 h-10'
+                  resizeMode='contain'
+                />
+              </View>
+            </View>
+
+            <SearchInput
+              placeholder='Search for a video'
+              value={searchTerm}
+              handleChangeText={handleChangeText}
+            />
+
+            <View className='w-full flex-1 pt-5 pb-8'>
+              <Text className='text-lg font-pregular text-gray-100 mb-3'>
+                Latest Videos
+              </Text>
+
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }]} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title='No Videos Found'
+            subtitle='Be the first one to upload a video'
+          />
+        )}
+        refreshControl={
+          <RefreshControl
+            refreshing={refresing}
+            onRefresh={onRefresh}
+          />
+        }
+      />
+    </SafeAreaView>
   );
 }
